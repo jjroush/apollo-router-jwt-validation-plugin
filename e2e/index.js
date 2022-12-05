fd812f30f98369b5ae811743f51ea6c40ec96e50
roushj <roushj@pella.com>
2022-12-05 10:02:05 -0600

    read header

--- e2e/index.js
+++ e2e/index.js
@@ -1,8 +1,43 @@
+import dns from 'node:dns';
+
 import { describe, test } from 'node:test';
 import assert from 'node:assert';
+dns.setDefaultResultOrder('ipv4first');
 
 describe('e2e', () => {
-    test('Server should start', () => {
-        assert.strictEqual(1, 1);
+    describe('when auth header is not provided', () => {
+        test('should successfully query server', async () => {
+            const {status} = await fetch('http://localhost:4000?query={__typename}', {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            assert.strictEqual(status, 200);
+        });
+    });
+
+    describe('when auth header is provided', () => {
+        test('should return unauthorized when Bearer prefix is not provided', async () => {
+            const {status} = await fetch('http://localhost:4000?query={__typename}', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+                }
+            });
+
+            assert.strictEqual(status, 401);
+        });
+
+        test('should successfully query when Bearer prefix is provided', async () => {
+            const {status} = await fetch('http://localhost:4000?query={__typename}', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+                }
+            });
+
+            assert.strictEqual(status, 200);
+        });
     });
 });
--- src/plugins/jwt_validation_plugin.rs
+++ src/plugins/jwt_validation_plugin.rs
@@ -1,11 +1,26 @@
+use std::ops::ControlFlow;
+use std::vec::Vec;
+
+use apollo_router::graphql;
+use apollo_router::layers::ServiceBuilderExt;
 use apollo_router::register_plugin;
 use apollo_router::plugin::PluginInit;
 use apollo_router::plugin::Plugin;
 use apollo_router::services::*;
+use apollo_router::Context;
+
+use reqwest::StatusCode;
+
 use schemars::JsonSchema;
+use base64::{decode};
 use serde::Deserialize;
-// use std::println;
-use tower::{util::BoxService, BoxError};
+use tower::{util::BoxService, BoxError, ServiceBuilder, ServiceExt};
+
+#[derive(Debug, Default, Deserialize, JsonSchema, Clone)]
+struct Authorizer {
+    name: String,
+    issuer: String
+}
 
 #[allow(dead_code)]
 struct JwtValidationPlugin {
@@ -14,7 +29,7 @@ struct JwtValidationPlugin {
 
 #[derive(Debug, Default, Deserialize, JsonSchema)]
 struct Conf {
-    // Put your plugin confguration here. It's deserialzed from YAML automatically.
+    authorizers: Vec<Authorizer>
 }
 
 #[async_trait::async_trait]
@@ -34,15 +49,116 @@ impl Plugin for JwtValidationPlugin {
         self: &JwtValidationPlugin,
         service: BoxService<supergraph::Request, supergraph::Response, BoxError>,
     ) -> BoxService<supergraph::Request, supergraph::Response, BoxError> {
-        service
-    }
+        let token_header: &'static str = "Authorization";
+        let authorizers = self.configuration.authorizers.clone();
 
-    // fn execution_service(
-    //     self: &JwtValidationPlugin,
-    //     service: BoxService<supergraph::Request, supergraph::Response, BoxError>,
-    // ) -> BoxService<supergraph::Request, supergraph::Response, BoxError> {
-    //     service
-    // }
+        let mut issuers: Vec<String> = Vec::new();
+
+        for i in 0..authorizers.len() {
+            issuers.push(authorizers[i].issuer.clone());
+        }
+
+
+        ServiceBuilder::new()
+            .checkpoint(move |req: supergraph::Request| {
+                fn failure_message(
+                    context: Context,
+                    msg: String,
+                    status: StatusCode,
+                ) -> Result<ControlFlow<supergraph::Response, supergraph::Request>, BoxError>
+                {
+                    let res = supergraph::Response::error_builder()
+                        .error(graphql::Error::builder().message(msg).build())
+                        .status_code(status)
+                        .context(context)
+                        .build()?;
+                    Ok(ControlFlow::Break(res))
+                }
+
+                let jwt_value_raw = match req.supergraph_request.headers().get(token_header) {
+                    Some(value) => value.to_str(),
+                    None =>
+                        {
+                            // Optional JWT
+                            return Ok(ControlFlow::Continue(req))
+                        }
+                };
+
+                let jwt_value = match jwt_value_raw {
+                    Ok(value) => value,
+                    Err(_not_a_string_error) => {
+                        // Prepare an HTTP 400 response with a GraphQL error message
+                        return failure_message(
+                            req.context,
+                            "Authorization' header is not convertible to a string".to_string(),
+                            StatusCode::BAD_REQUEST,
+                        );
+                    }
+                };
+
+                if !jwt_value
+                    .starts_with(&format!("{} ", "Bearer"))
+                {
+                    // Prepare an HTTP 400 response with a GraphQL error message
+                    return failure_message(
+                        req.context,
+                        "Header is not correctly formatted".to_string(),
+                        StatusCode::UNAUTHORIZED,
+                    );
+                }
+
+                let jwt_parts: Vec<&str> = jwt_value.splitn(2, ' ').collect();
+
+                if jwt_parts.len() != 2 {
+                    // Prepare an HTTP 400 response with a GraphQL error message
+                    return failure_message(
+                        req.context,
+                        "Header is not correctly formatted".to_string(),
+                        StatusCode::UNAUTHORIZED,
+                    );
+                };
+
+                let jwt: &str = jwt_parts[1];
+
+                let splitted_jwt_strings: Vec<_> = jwt.split('.').collect();
+
+                let jwt_payload_base64 = match splitted_jwt_strings.get(1).clone() {
+                    Some(value) => value,
+                    None => {
+                        // Prepare an HTTP 400 response with a GraphQL error message
+                        return failure_message(
+                            req.context,
+                            "Header is not correctly formatted".to_string(),
+                            StatusCode::UNAUTHORIZED,
+                        );
+                    }
+                };
+
+                let jwt_payload_decoded_raw = base64::decode(jwt_payload_base64)?;
+
+                let jwt_payload_decoded = String::from_utf8(jwt_payload_decoded_raw)?;
+
+
+
+                // let jwt_payload_base64 = match jwt_value_raw {
+                //     Ok(value) => value,
+                //     Err(_not_a_string_error) => {
+                //         // Prepare an HTTP 400 response with a GraphQL error message
+                //         return failure_message(
+                //             req.context,
+                //             "Authorization' header is not convertible to a string".to_string(),
+                //             StatusCode::BAD_REQUEST,
+                //         );
+                //     }
+                // };
+
+                println!("{:?}", jwt_payload_decoded);
+
+                Ok(ControlFlow::Continue(req))
+            })
+            .service(service)
+            .boxed()
+    }
 
     // Unlike other hooks, this hook also passes the name of the subgraph
     // being invoked. That's because this service might invoke *multiple*
